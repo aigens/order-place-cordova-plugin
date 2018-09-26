31799e5225d72311744cc61ab684b53d53bdf91f
Peter Liu <peter.liu@aigens.com>
2018-09-26 18:32:42 +0800

    add swift code

---
+++ src/ios/OrderPlaceCordovaPlugin.swift
@@ -0,0 +1,25 @@
+@objc(OrderPlaceCordovaPlugin) class OrderPlaceCordovaPlugin : CDVPlugin {
+    
+    func openUrl(command: CDVInvokedUrlCommand) {
+        
+        /*
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        */
+        print("openUrl")
+        
+        var result: [String: String] = [:]
+            
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: result
+        )
+        
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+}
