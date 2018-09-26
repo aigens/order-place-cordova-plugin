d05a9003206cfaaf4afcb4108c60c58cf9e6c4aa
Peter Liu <peter.liu@aigens.com>
2018-09-26 19:07:59 +0800

    add scan

--- src/ios/OrderPlaceCordovaPlugin.swift
+++ src/ios/OrderPlaceCordovaPlugin.swift
@@ -1,21 +1,49 @@
+import OrderPlaceSdk
+import UIKit
+
 @objc(OrderPlaceCordovaPlugin) class OrderPlaceCordovaPlugin : CDVPlugin {
     
     func openUrl(_ command: CDVInvokedUrlCommand) {
+       
+        let url = command.argument(at: 0) as! String
+        let features = command.argument(at: 1) as! NSArray
         
-        /*
-        var pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_ERROR
-        )
-        */
-        print("openUrl")
-        
-        var result: [String: String] = [:]
+        let result: [String: String] = [:]
             
-        var pluginResult = CDVPluginResult(
+        let pluginResult = CDVPluginResult(
             status: CDVCommandStatus_OK,
             messageAs: result
         )
         
+        let featureString = features.componentsJoined(by: ",")
+        
+        print("openUrl", url, featureString)
+        
+        OrderPlace.openUrl(caller: self.viewController, url: url, features: featureString)
+        
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    
+    
+    func scan(_ command: CDVInvokedUrlCommand) {
+        
+        let features = command.argument(at: 0) as! NSArray
+        
+        let result: [String: String] = [:]
+        
+        let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: result
+        )
+        
+        let featureString = features.componentsJoined(by: ",")
+        
+        print("scan", featureString)
+        
+        OrderPlace.scan(caller: self.viewController, features: featureString)
         
         self.commandDelegate!.send(
             pluginResult,
