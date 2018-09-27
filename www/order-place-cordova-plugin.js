e22c2ba95232df3eeeb4f47c37d980fc37598de6
Peter Liu <peter.liu@aigens.com>
2018-09-27 09:57:02 +0800

    change method sig

--- www/order-place-cordova-plugin.js
+++ www/order-place-cordova-plugin.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.openUrl = function (success, error, args) {
-    exec(success, error, 'order-place-cordova-plugin', 'openUrl', args);
+exports.openUrl = function (success, error, params) {
+    exec(success, error, 'order-place-cordova-plugin', 'openUrl', [params]);
 };
 
-exports.scan = function (success, error, args) {
-    exec(success, error, 'order-place-cordova-plugin', 'scan', args);
+exports.scan = function (success, error, params) {
+    exec(success, error, 'order-place-cordova-plugin', 'scan', [params]);
 };
