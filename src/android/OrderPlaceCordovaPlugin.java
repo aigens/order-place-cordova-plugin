b0b3592594703b7c28f246a8ce6687fdf6cad4c8
mingaigenscn <ming.lin.huang@aigens.cn>
2019-04-09 15:53:22 +0800

    0.1.10 add back() with params

--- src/android/OrderPlaceCordovaPlugin.java
+++ src/android/OrderPlaceCordovaPlugin.java
@@ -1,12 +1,16 @@
 package com.aigens.sdk.plugin;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.aigens.sdk.OrderPlace;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,13 +22,13 @@ import java.util.List;
 public class OrderPlaceCordovaPlugin extends CordovaPlugin {
 
     private static String TAG = "SdkOrderPlaceCordova";
+    private CallbackContext callbackContext;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
 
-
-
-        if(action.equals("openUrl")){
+        if (action.equals("openUrl")) {
 
 
             JSONObject params = args.optJSONObject(0);
@@ -33,12 +37,12 @@ public class OrderPlaceCordovaPlugin extends CordovaPlugin {
 
             OrderPlace.openUrl(cordova.getActivity(), url, params);
 
-        }else if(action.equals("scan")){
+        } else if (action.equals("scan")) {
 
             JSONObject params = args.optJSONObject(0);
 
             OrderPlace.scan(cordova.getActivity(), params);
-        }else{
+        } else {
 
             //callbackContext.error("no method " + action);
             return false;
@@ -48,11 +52,55 @@ public class OrderPlaceCordovaPlugin extends CordovaPlugin {
         result.put("status", "success");
 
         Log.d(TAG, "execute:" + action);
-        callbackContext.success(result.toString());
 
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
         return true;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
 
+        if (this.callbackContext != null) {
+            if (resultCode == Activity.RESULT_OK) {
+                JSONObject result = new JSONObject();
 
+                switch (requestCode) {
+                    case (OrderPlace.REQUEST_CODE_OPEN_URL):
+                        try {
+                            result.put("closeData", intent.getStringExtra("closeData"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, result);
+                        pluginResult.setKeepCallback(true);
+                        callbackContext.sendPluginResult(pluginResult);
+                        break;
+                    case (OrderPlace.REQUEST_CODE_SCAN):
+                        try {
+                            result.put("closeData", intent.getStringExtra("closeData"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        PluginResult pluginResult2 = new PluginResult(PluginResult.Status.OK, result);
+                        pluginResult2.setKeepCallback(true);
+                        callbackContext.sendPluginResult(pluginResult2);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                this.callbackContext.error("Unexpected error");
+            }
+        }
+
+    }
+
+    public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+    }
 }
--- src/ios/OrderPlaceCordovaPlugin.swift
+++ src/ios/OrderPlaceCordovaPlugin.swift
@@ -4,10 +4,12 @@ import OrderPlaceSDK
 
 
 @objc(OrderPlaceCordovaPlugin) class OrderPlaceCordovaPlugin : CDVPlugin {
+    var callbackId:String? = nil;
     
     @objc(openUrl:) func openUrl(_ command: CDVInvokedUrlCommand) {
        
         guard let params = command.argument(at: 0) as? [String: Any] else {return}
+        self.callbackId = command.callbackId;
        
         let result: [String: String] = [:]
             
@@ -15,13 +17,24 @@ import OrderPlaceSDK
             status: CDVCommandStatus_OK,
             messageAs: result
         )
-        
+        pluginResult?.keepCallback = NSNumber(value: true);
         //let featureString = features.componentsJoined(by: ",")
-        guard let url = params["url"] as? String else {return}
+        guard var url = params["url"] as? String else {return}
+        
+//        url = "http://192.168.0.237:8100/#/store/102945/mode/takeaway";
         
         print("openUrl", params)
         
-        OrderPlace.openUrl(caller: self.viewController, url: url, options: params)
+        //OrderPlace.openUrl(caller: self.viewController, url: url, options: params)
+        OrderPlace.openUrl(caller: self.viewController, url: url, options: params) { [weak self] (result) in
+            print("openUrl result:\(result)")
+            if let values = result as? [String: Any] {
+                print("emitEvent:\(values)")
+                var dict = [String: Any]();
+                dict["closeData"] = values;
+                self?.emitEvent(dict);
+            }
+        }
         
         self.commandDelegate!.send(
             pluginResult,
@@ -29,6 +42,16 @@ import OrderPlaceSDK
         )
     }
     
+    private func emitEvent(_ event: [String: Any]) {
+        if let cbID = self.callbackId {
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: event);
+            pluginResult?.keepCallback = NSNumber(value: true);
+            self.commandDelegate?.send(
+                pluginResult,
+                callbackId: cbID
+            )
+        }
+    }
     
     @objc(scan:) func scan(_ command: CDVInvokedUrlCommand) {
         
